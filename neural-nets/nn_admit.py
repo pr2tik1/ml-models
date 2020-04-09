076f97516924dbb879bea57c34a17687b24e7dd4
pr2tik1 <pr2tik1@gmail.com>
2020-04-09 23:38:56 +0530

    admit n/n update

--- neural-nets/nn_admit.py
+++ neural-nets/nn_admit.py
@@ -3,17 +3,8 @@ from data_prep import features, targets, features_test, targets_test
 
 
 def sigmoid(x):
-    """
-    Calculate sigmoid
-    """
     return 1 / (1 + np.exp(-x))
 
-# TODO: We haven't provided the sigmoid_prime function like we did in
-#       the previous lesson to encourage you to come up with a more
-#       efficient solution. If you need a hint, check out the comments
-#       in solution.py from the previous lecture.
-
-# Use to same seed to make debugging easier
 np.random.seed(42)
 
 n_records, n_features = features.shape
@@ -29,29 +20,15 @@ learnrate = 0.5
 for e in range(epochs):
     del_w = np.zeros(weights.shape)
     for x, y in zip(features.values, targets):
-        # Loop through all records, x is the input, y is the target
+        output = sigmoid(np.dot(x, weights))
 
-        # Note: We haven't included the h variable from the previous
-        #       lesson. You can add it if you want, or you can calculate
-        #       the h together with the output
+        error = y - output
 
-        # TODO: Calculate the output
-        output = None
+        error_term = error * output * (1 - output)
+        del_w += error_term * x
 
-        # TODO: Calculate the error
-        error = None
+    weights += learnrate * del_w / n_records
 
-        # TODO: Calculate the error term
-        error_term = None
-
-        # TODO: Calculate the change in weights for this sample
-        #       and add it to the total weight change
-        del_w += 0
-
-    # TODO: Update weights using the learning rate and the average change in weights
-    weights += 0
-
-    # Printing out the mean square error on the training set
     if e % (epochs / 10) == 0:
         out = sigmoid(np.dot(features, weights))
         loss = np.mean((out - targets) ** 2)
@@ -62,7 +39,6 @@ for e in range(epochs):
         last_loss = loss
 
 
-# Calculate accuracy on test data
 tes_out = sigmoid(np.dot(features_test, weights))
 predictions = tes_out > 0.5
 accuracy = np.mean(predictions == targets_test)
