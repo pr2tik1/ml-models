4ba674aaa261f1fb033b6a8467ee488d3d53bca4
pr2tik1 <pr2tik1@gmail.com>
2020-05-15 10:05:18 +0530

    accuracy increased

--- neural-nets/movie-sentiment/model.py
+++ neural-nets/movie-sentiment/model.py
@@ -17,17 +17,8 @@ class SentimentNetwork:
             learning_rate(float) - Learning rate to use while training
         
         """
-        # Assign a seed to our random number generator to ensure we get
-        # reproducable results during development 
         np.random.seed(1)
-
-        # process the reviews and their associated labels so that everything
-        # is ready for training
         self.pre_process_data(reviews, labels, polarity_cutoff, min_count)
-        
-        # Build the network to have the number of hidden nodes and the learning rate that
-        # were passed into this initializer. Make the same number of input nodes as
-        # there are vocabulary words and create a single output node.
         self.init_network(len(self.review_vocab),hidden_nodes, 1, learning_rate)
 
     def pre_process_data(self, reviews, labels, polarity_cutoff, min_count):
@@ -68,22 +59,15 @@ class SentimentNetwork:
                     else:
                         review_vocab.add(word)
 
-        # Convert the vocabulary set to a list so we can access words via indices
         self.review_vocab = list(review_vocab)
-        
-        # populate label_vocab with all of the words in the given labels.
         label_vocab = set()
         for label in labels:
             label_vocab.add(label)
         
-        # Convert the label vocabulary set to a list so we can access labels via indices
         self.label_vocab = list(label_vocab)
-        
-        # Store the sizes of the review and label vocabularies.
         self.review_vocab_size = len(self.review_vocab)
         self.label_vocab_size = len(self.label_vocab)
         
-        # Create a dictionary of words in the vocabulary mapped to index positions
         self.word2index = {}
         for i, word in enumerate(self.review_vocab):
             self.word2index[word] = i
@@ -94,24 +78,15 @@ class SentimentNetwork:
             self.label2index[label] = i
 
     def init_network(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
-        # Set number of nodes in input, hidden and output layers.
+        
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
-
-        # Store the learning rate
         self.learning_rate = learning_rate
 
-        # Initialize weights
-
-        # These are the weights between the input layer and the hidden layer.
         self.weights_0_1 = np.zeros((self.input_nodes,self.hidden_nodes))
-
-        # These are the weights between the hidden layer and the output layer.
-        self.weights_1_2 = np.random.normal(0.0, self.output_nodes**-0.5, 
+        self.weights_1_2 = np.random.normal(0.0, self.hidden_nodes**-0.5, 
                                                 (self.hidden_nodes, self.output_nodes))
-        
-        # The input layer, a two-dimensional matrix with shape 1 x hidden_nodes
         self.layer_1 = np.zeros((1,hidden_nodes))
     
     def get_target_for_label(self,label):
@@ -119,17 +94,30 @@ class SentimentNetwork:
             return 1
         else:
             return 0
-        
+    
+    def tanh(self,x):
+        t=(np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+        return t
+    
+    def tanh_derivative(self,t):
+        dt=1-t**2
+        return dt
+    
     def sigmoid(self,x):
         return 1 / (1 + np.exp(-x))
     
     def sigmoid_output_2_derivative(self,output):
         return output * (1 - output)
     
+    def relu(self, x):
+        return np.maximum(0,x)
+    
+    def reluDerivative(self, x):
+        x[x<=0] = 0 
+        x[x>0] = 1
+        return x
+    
     def train(self, training_reviews_raw, training_labels):
-        train_loss = 0.0
-        valid_loss = 0.0
-        
         training_reviews = list()
         for review in training_reviews_raw:
             indices = set()
@@ -138,21 +126,11 @@ class SentimentNetwork:
                     indices.add(self.word2index[word])
             training_reviews.append(list(indices))
 
-        # make sure out we have a matching number of reviews and labels
         assert(len(training_reviews) == len(training_labels))
-        
-        # Keep track of correct predictions to display accuracy during training 
         correct_so_far = 0
-
-        # Remember when we started for printing time statistics
         start = time.time()
         
-        # loop through all the given reviews and run a forward and backward pass,
-        # updating weights for every item
-        
         for i in range(len(training_reviews)):
-            
-            # Get the next review and its correct label
             review = training_reviews[i]
             label = training_labels[i]
             
@@ -162,9 +140,7 @@ class SentimentNetwork:
 
             # Output layer
             layer_2 = self.sigmoid(self.layer_1.dot(self.weights_1_2))            
-            
             ### Backward pass ###
-
             # Output error
             layer_2_error = layer_2 - self.get_target_for_label(label) # Output layer error is the difference between desired target and actual output.
             layer_2_delta = layer_2_error * self.sigmoid_output_2_derivative(layer_2)
@@ -174,10 +150,10 @@ class SentimentNetwork:
             layer_1_delta = layer_1_error # hidden layer gradients - no nonlinearity so it's the same as the error
 
             # Update the weights
-            self.weights_1_2 -= self.layer_1.T.dot(layer_2_delta) * self.learning_rate # update hidden-to-output weights with gradient descent step
+            self.weights_1_2 -= self.layer_1.T.dot(layer_2_delta) * self.learning_rate 
             
             for index in review:
-                self.weights_0_1[index] -= layer_1_delta[0] * self.learning_rate # update input-to-hidden weights with gradient descent step
+                self.weights_0_1[index] -= layer_1_delta[0] * self.learning_rate 
 
             # Keep track of correct predictions.
             if(layer_2 >= 0.5 and label == 'POSITIVE'):
@@ -203,14 +179,10 @@ class SentimentNetwork:
         and uses the test_labels to calculate the accuracy of those predictions.
         """
         
-        # keep track of how many correct predictions we make
         correct = 0
 
-        # we'll time how many predictions per second we make
         start = time.time()
 
-        # Loop through each of the given reviews and call run to predict
-        # its label. 
         for i in range(len(testing_reviews)):
             pred = self.run(testing_reviews[i])
             if(pred == testing_labels[i]):
@@ -231,9 +203,6 @@ class SentimentNetwork:
         """
         Returns a POSITIVE or NEGATIVE prediction for the given review.
         """
-        # Run a forward pass through the network, like in the "train" function.
-        # Hidden layer
-
         self.layer_1 *= 0
         unique_indices = set()
         for word in review.lower().split(" "):
@@ -243,11 +212,8 @@ class SentimentNetwork:
             self.layer_1 += self.weights_0_1[index]
         
         # Output layer
-        ## New for Project 5: changed to use self.layer_1 instead of local layer_1
         layer_2 = self.sigmoid(self.layer_1.dot(self.weights_1_2))
          
-        # Return POSITIVE for values above greater-than-or-equal-to 0.5 in the output layer;
-        # return NEGATIVE for other values
         if(layer_2[0] >= 0.5):
             return "POSITIVE"
         else:
