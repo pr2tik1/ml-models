7adac08cd31adc2356c264c11c8e8596935455eb
pratikkumar04 <pratik.kumar@elucidata.io>
2021-06-13 14:50:10 +0530

    dir changed

---
+++ ml-models/linear_regression.py
@@ -0,0 +1,20 @@
+import numpy as np 
+
+class linear_regression:
+    """
+    Compute Linear Regression for 
+    """
+    def __init__(self, values):
+        self.values = values
+
+
+
+
+
+    
+if __name__ == "__main__":
+
+    X= 
+    y=
+    reg = linear_regression()
+    w=
